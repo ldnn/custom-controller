d6c55b7f045c8a93c2d73911530b4766d41084ea
ldnn <ldnn@outlook.com>
2023-09-11 15:48:21 +0800

    first commit

---
+++ internal/controller/namespace_controller.go
@@ -0,0 +1,103 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+// NamespaceReconciler reconciles a Namespace object
+type NamespaceReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+//+kubebuilder:rbac:groups=ks.cloud.cmft.ks.cloud.cmft,resources=namespaces,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=ks.cloud.cmft.ks.cloud.cmft,resources=namespaces/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=ks.cloud.cmft.ks.cloud.cmft,resources=namespaces/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the Namespace object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.4/pkg/reconcile
+func (r *NamespaceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	_ = log.FromContext(ctx)
+
+	// TODO(user): your logic here
+	namespace := &corev1.Namespace{}
+	if err := r.Get(ctx, req.NamespacedName, namespace); err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+	var mdAn, mdLb bool
+	namespace, mdAn = LabelTuner("kubesphere.io/logs", namespace)
+	namespace, mdLb = LabelTuner("apm-injection", namespace)
+	if mdAn || mdLb {
+		if err := r.Update(ctx, namespace); err != nil {
+			return ctrl.Result{}, err
+		}
+	}
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *NamespaceReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		// Uncomment the following line adding a pointer to an instance of the controlled resource as an argument
+		// For().
+		For(&corev1.Namespace{}).
+		Owns(&corev1.Namespace{}).
+		Complete(r)
+}
+
+func LabelTuner(key string, namespace *corev1.Namespace) (*corev1.Namespace, bool) {
+
+	if namespace.Labels == nil {
+		namespace.Labels = make(map[string]string)
+	}
+
+	modfiy := false
+
+	if _, ok := namespace.Annotations[key]; !ok {
+		if _, ok := namespace.Labels[key]; ok {
+			delete(namespace.Labels, key)
+			modfiy = true
+		}
+	} else {
+		if _, ok := namespace.Labels[key]; !ok {
+			namespace.Labels[key] = namespace.Annotations[key]
+			modfiy = true
+		} else {
+			if namespace.Labels[key] != namespace.Annotations[key] {
+				namespace.Labels[key] = namespace.Annotations[key]
+				modfiy = true
+			}
+		}
+	}
+	return namespace, modfiy
+}
